b7fcce895eb15a5da2bfb51e2f98d0dfe0ec1384
NYA Zakaria <zakaria.nya@hetic.net>
2019-03-02 11:23:53 +0100

    FEAT ajoute un crud avec mongodb sur notre model schema de blogpost

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const api = require('./api/v1/index');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const mongoose = require('mongoose');
 const connection = mongoose.connection;
 
 app.set('port', (process.env.port || 3000));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 
@@ -17,9 +20,9 @@ app.use(cors());
 	
 	app.use('/api/v1', api);
 const use = app.use((req, res) =>{
-	const err = new Error('404 - not found ');
-	err.status = 404;
-	res.json({msg: 'not found', err: err});
+	const err = new Error(' 404- not found ');
+	err.status = 500;
+	res.json({msg: '404 - not found', err: err});
 });
 
 mongoose.connect('mongodb://localhost:27017/cmsbis', 
